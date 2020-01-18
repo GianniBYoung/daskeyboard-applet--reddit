afb13fb765f3959041580418237cb64c692fab2e
Ben Reisen <breisen99@gmail.com>
2020-01-18 17:39:17 -0600

    Many changes

--- index.js
+++ index.js
@@ -1,52 +1,12 @@
-//'use strict';
-//const request = require('request-promise');
-//const snoowrap = require('snoowrap');
+const request = require('request-promise');
 const q = require('daskeyboard-applet');
 const logger = q.logger;
 
 /**
- *
- const r = new snoowrap({
-  // userAgent: creds.userAgent,
-  // clientId: creds.clientId,
-  // clientSecret: creds.clientSecret,
-  // refreshToken: creds.refreshToken
-  userAgent: 'copmuter',
-      clientId: '81RAdGNDkcEjow',
-      clientSecret: 'yeG6ghwpfTXh7bmVS4n6K5zCur0',
-      refreshToken: '429012468801-_EAaoeTkviXImAvm2-YQyV0j_nM',
-});
-*
-**/
+ * Retrieve data from the service
+ */
+ async run() {
+   new q.Point('#0000FF',q.Effects.BLINK)
+ }
 
-
-//if (r.getUser('Tight-Document').has_mail) {
-//  console.log("You have mail!");
-//}
-
-
-
-class RedditNotification extends q.DesktopApp {
-  constructor() {
-    super();
-    // store a record of previously notified deals
-    //this.notified = {};
-    // run every 5 min
-    //this.pollingInterval = 5 * 60 * 1000;
-  }
-
-  async run() {
-    return new q.Signal({
-      points: [
-        [
-          new q.Point('#FF0000',q.Effects.BLINK)
-        ]
-      ]
-    });
-}
-
-module.exports = {
-  RedditNotification: RedditNotification
-}
-
-const applet = new RedditNotification();
+const applet = new TwitchStreams();
