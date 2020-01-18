7d3b03662eb002fc21b583c7f497f0d5d6863956
Nick Burbach <nburbach@huskers.unl.edu>
2020-01-18 16:13:10 -0600

    Initial test code for signal lights

--- index.js
+++ index.js
@@ -15,4 +15,78 @@ const r = new snoowrap({
 });
 
 
-r.getInbox().then(console.log)
+if (r.getUser('Tight-Document').has_mail) {
+  console.log("You have mail!");
+}
+
+const request = require('request-promise');
+const q = require('daskeyboard-applet');
+const logger = q.logger;
+
+
+class RedditNotification extends q.DesktopApp {
+  constructor() {
+    super();
+    // store a record of previously notified deals
+    this.notified = {};
+    // run every 5 min
+    this.pollingInterval = 5 * 60 * 1000;
+  }
+
+  async applyConfig() {
+    const userLogins = this.config.userLogins;
+
+    if (userLogins) {
+      logger.info("My user logins are: " + JSON.stringify(userLogins));
+
+      for (let login of userLogins) {
+        if (null == login || login.trim() == '') {
+          throw new Error("User logins must not be empty.")
+        }
+      }
+    }
+  }
+
+
+generateSignal(data) {
+    for (let stream of data.data) {
+        return new q.Signal({
+          points: [
+            [
+              new q.Point('#0000FF',q.Effects.BLINK)
+            ]
+          ]
+        });
+      }
+    return null;
+  }
+
+  async run() {
+    const userLogins = this.config.userLogins;
+
+    if (userLogins) {
+      logger.info("My user logins are: " + JSON.stringify(userLogins));
+
+      return retrieveData(userLogins)
+        .then(body => {
+          return this.generateSignal(body);
+        }).catch(error => {
+          logger.error(`Error while getting Twitch data: ${error}`);
+          if(`${error.message}`.includes("getaddrinfo")){
+          }else{
+            return q.Signal.error([`The Reddit service returned an error. Detail: ${error}`]);
+          }
+        });
+    } else {
+      logger.warn("No userLogins configured.");
+      return null;
+    }
+  }
+}
+
+module.exports = {
+  RedditNotification: RedditNotification,
+  retrieveData: retrieveData,
+}
+
+const applet = new RedditNotification();
